a20f274206adc4cbdc1ca08b14d9a1aa6c36be1b
Your Name <you@example.com>
2022-12-26 19:00:45 +0000

    more stuff

--- after/plugin/neovim.lua
+++ after/plugin/neovim.lua
@@ -1,1 +1,5 @@
+-- Buffers
+vim.keymap.set("n", "<leader>bc", "<cmd>bd<cr>", { desc = "[B]uffer [C]lose"})
 vim.keymap.set("n", "<leader>bt", "<cmd>tab split<cr>", { desc = "[B]uffer into new [T]ab"})
+vim.keymap.set("n", "<leader>bv", "<cmd>vsplit<cr>", { desc = "[B]uffer [V]ertical Split"})
+vim.keymap.set("n", "<leader>bh", "<cmd>split<cr>", { desc = "[B]uffer [H]orizontal Split"})
